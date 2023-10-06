78d328a9041de63bfcbb2e1e8df697d74d7148d1
rosol111 <gulamrosul037@gmail.com>
2023-10-06 21:17:13 +0600

    Added by Breaking news slider using React Marquee and navbar

--- src/pages/shared/Heaader/Header.jsx
+++ src/pages/shared/Heaader/Header.jsx
@@ -1,7 +1,12 @@
+import logo from "../../../assets/logo.png";
+import moment from "moment";
+
 const Header = () => {
   return (
-    <div>
-      <h2 className="text-3xl">Header</h2>
+    <div className="text-center mt-6 mb-4">
+      <img className="mx-auto" src={logo} alt="" />
+      <p className="mt-4">Journalism Without Fear or Favour</p>
+      <p className="text-xl mt-4">{moment().format("MMMM D, YYYY")}</p>
     </div>
   );
 };
